ab4a1407f5d86f9e65a67447e1fc4b8c80e0b4a6
Wichert Akkerman <wichert@wiggy.net>
2011-06-15 23:42:06 +0800

    Add classifiers.
    committer: Marcin Lulek <info@webreactor.eu>
    
    --HG--
    extra : convert_revision : 72b27574b0c03b19a817dc5ab74d8c5e7980ea07

--- setup.py
+++ setup.py
@@ -3,7 +3,17 @@ from setuptools import setup
 
 setup(name='errormator_client',
       version='0.2',
-      description=""" Sample client for Errormator reporting""",
+      description='Sample client for Errormator reporting',
+      classifiers=[
+          'Intended Audience :: Developers',
+          'License :: DFSG approved',
+          'License :: OSI Approved :: BSD License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+          ],
       author='Marcin Lulek',
       author_email='info@webreactor.eu',
       license='BSD',
