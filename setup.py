57866a83e7ac9412ad1ffe9cefe5f69fff2a7d32
Wichert Akkerman <wichert@wiggy.net>
2011-06-15 23:41:59 +0800

    PEP8, add mising requirement.
    committer: Marcin Lulek <info@webreactor.eu>
    
    --HG--
    extra : convert_revision : bed1faaf530f1ef7250cd51e66a95b6eb45ca19e

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
-from setuptools import setup, find_packages
-import sys
+from setuptools import setup
+
+
 setup(name='errormator_client',
       version='0.2',
       description=""" Sample client for Errormator reporting""",
       author='Marcin Lulek',
       author_email='info@webreactor.eu',
       license='BSD',
-      py_modules = ['errormator_client'],
+      py_modules=['errormator_client'],
+      install_requires=[
+          'Paste',
+      ],
       entry_points="""
       [paste.filter_app_factory]
       error_catcher = errormator_client:make_catcher_middleware
