6a77fb9522537603f94e6fd5b7d5bf80c8c2ca5d
ergo <none@none>
2012-06-26 13:13:08 +0200

    more debug levels

--- errormator_client/utils.py
+++ errormator_client/utils.py
@@ -32,7 +32,7 @@ def import_module(name):
     try:
         return __import__(name, globals(), locals(), [], -1)
     except ImportError as e:
-        log.warning('Could not import module: %s' % e)
+        log.debug('Could not import module: %s' % e)
 
 def import_from_module(name):
     try:
@@ -40,7 +40,7 @@ def import_from_module(name):
         _tmp = __import__(parts[0], globals(), locals(), [parts[1], ], -1)
         return getattr(_tmp, parts[1])
     except ImportError as e:
-        log.warning('Could not import from module: %s' % e)
+        log.debug('Could not import from module: %s' % e)
         
 def deco_func_or_method(module, name, deco_f, gatherer, min_duration):
     _tmp = name.split('.')
