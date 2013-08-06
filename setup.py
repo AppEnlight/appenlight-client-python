2c57ec3acb251d912f106765f82cf34b9cef4859
Marcin Lulek <info@webreactor.eu>
2013-08-06 19:09:41 +0200

    bump revision

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='errormator_client',
-      version='0.5.12',
+      version='0.5.13',
       description='Client for Errormator reporting - supporting WSGI and django (http://errormator.com)',
       classifiers=[
           'Intended Audience :: Developers',
