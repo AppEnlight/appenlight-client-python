635d1dd32b0c9462ebe79eb9f8c4b3111fc6ce76
Marcin Lulek <info@webreactor.eu>
2013-12-10 18:55:50 +0100

    add support for flask

--- appenlight_client/ext/flask.py
+++ appenlight_client/ext/flask.py
@@ -3,6 +3,7 @@ from flask import request
 from flask.signals import got_request_exception, request_started
 from appenlight_client.client import make_appenlight_middleware, get_config
 from appenlight_client.ext.general import gather_data
+from appenlight_client.utils import fullyQualifiedName
 
 import logging
 
@@ -20,6 +21,11 @@ def populate_post_vars(sender, **extra):
     This is to handle iterated wsgi.input by werkzeug when we create webob obj 
     parsing environ
     """
+    try:
+        view_callable = sender.view_functions[request.endpoint]
+        request.environ['appenlight.view_name'] = fullyQualifiedName(view_callable)
+    except Exception, e:
+        pass
     if request.method in ['POST', 'PUT']:
         request.environ['appenlight.post_vars'] = request.form
     else:
