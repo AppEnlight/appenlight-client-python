e61465fe0913b93d92f838254fde2dea303544a2
Marcin Lulek <info@webreactor.eu>
2013-12-12 23:17:00 +0100

    proper names for class views

--- appenlight_client/ext/pyramid_tween.py
+++ appenlight_client/ext/pyramid_tween.py
@@ -7,10 +7,22 @@ import pkg_resources
 import pyramid.config
 import pyramid
 import logging
+import inspect
 
 from functools import wraps
 
 log = logging.getLogger(__name__)
+
+# stolen from pyramid 1.4
+def combine(*decorators):
+    def decorated(view_callable):
+        # reversed() is allows a more natural ordering in the api
+        for decorator in reversed(decorators):
+            view_callable = decorator(view_callable)
+        return view_callable
+
+    return decorated
+
 
 # feature sniffing
 pyramid_version_str = pkg_resources.get_distribution('pyramid').version
@@ -24,14 +36,39 @@ if pyramid_version > 1.4 or pyramid_version == 1.4 and test_ver not in ['a0', 'a
     can_append_decorator = True
 
 
+def wrap_pyramid_view_method_name(appenlight_callable):
+    """This add missing methods to name"""
+
+    @wraps(appenlight_callable)
+    def view_callable_wrapper(*args, **kwargs):
+        appenlight_storage = get_local_storage(local_timing)
+        if hasattr(appenlight_storage, 'view_name'):
+            try:
+                appenlight_storage.view_name = '%s.%s' % (appenlight_storage.view_name,
+                                                          appenlight_callable.__name__)
+            except Exception, e:
+                pass
+        return appenlight_callable(*args, **kwargs)
+
+    return view_callable_wrapper
+
+
 def wrap_pyramid_view_name(appenlight_callable):
     @wraps(appenlight_callable)
     def view_callable_wrapper(context, request):
         appenlight_storage = get_local_storage(local_timing)
+        view_name = ''
         try:
-            view_name = fullyQualifiedName(appenlight_callable)
+            original_view = getattr(appenlight_callable, '__original_view__')
+            if original_view:
+                view_name = fullyQualifiedName(appenlight_callable)
+                if not hasattr(original_view, '_appenlight_name') and inspect.isclass(original_view):
+                    original_view._appenlight_name = view_name
+                    for k, v in original_view.__dict__.items():
+                        if not k.startswith('_'):
+                            setattr(original_view, k, wrap_pyramid_view_method_name(v))
         except Exception, e:
-            view_name = ''
+            raise
         if 'pyramid/static' in view_name:
             #normalize static views
             view_name = 'pyramid/static'
@@ -61,6 +98,7 @@ def appenlight_tween_factory(handler, registry):
     def error_tween(request):
         try:
             response = handler(request)
+            appenlight_storage = get_local_storage(local_timing)
         except blacklist as e:
             raise
         except:
