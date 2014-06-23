5affdecf9a25e59b1a3f5a830ec5bef14527bd99
Marcin Lulek <info@webreactor.eu>
2014-06-23 11:38:09 +0200

    bump revision

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='appenlight_client',
-      version='0.6.7',
+      version='0.6.8',
       description='Client for Appenlight reporting - supporting WSGI and django (http://appenlight.com)',
       classifiers=[
           'Intended Audience :: Developers',
