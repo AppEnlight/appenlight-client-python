c16b22a18d163ede6b79f1208458df2234c7b7fe
Marcin Lulek <info@webreactor.eu>
2016-07-06 11:22:03 +0200

    pytest: add pytest.ini

--- appenlight_client/cli.py
+++ appenlight_client/cli.py
@@ -1,4 +1,4 @@
-from __future__ import with_statement
+from __future__ import with_statement, print_function
 import os
 import pkg_resources
 import sys
@@ -14,7 +14,7 @@ class CommandRouter(object):
     @classmethod
     def makeini(cls, ini_name):
         while True:
-            print '\nCurrent directory: %s' % cwd
+            print('\nCurrent directory: %s' % cwd)
             question = '''\nThere is no ini file in current directory,
                         create one? y/n: '''
             confirm = raw_input(question).lower()
@@ -28,7 +28,7 @@ class CommandRouter(object):
             ini_path = os.path.join(cwd, ini_name)
             exists = os.path.exists(ini_path)
             if exists:
-                print '\nFile %s already exists' % ini_path
+                print('\nFile %s already exists' % ini_path)
             else:
                 ini_str = pkg_resources.resource_string(
                     'appenlight_client',
@@ -38,29 +38,29 @@ class CommandRouter(object):
                         f.write(ini_str.decode('utf8'))
                     else:
                         f.write(ini_str)
-                print '\nCreated new appenlight client config: %s' % ini_path
-                print 'REMEMBER TO UPDATE YOUR API KEY IN INI FILE'
+                print('\nCreated new appenlight client config: %s' % ini_path)
+                print('REMEMBER TO UPDATE YOUR API KEY IN INI FILE')
 
-        print "\n\nFinished"
+        print("\n\nFinished")
 
     @classmethod
     def testini(cls, ini_name):
-        print '\nCurrent directory: %s' % cwd
+        print('\nCurrent directory: %s' % cwd)
         ini_path = os.path.join(cwd, ini_name)
         config = client.get_config(path_to_config=ini_path)
-        print 'INI file read - creating client'
+        print('INI file read - creating client')
         appenlight_client = client.Client(config)
-        print 'Client created, sending test entry'
+        print('Client created, sending test entry')
         logging.error('This is a test entry', extra={'sometag': 'appenlight.client.test',
                                                      'foo': 'bar'})
         records = appenlight_client.log_handlers_get_records()
         appenlight_client.py_log({}, records)
         result = appenlight_client.transport.send(appenlight_client.transport.log_queue[:], 'logs')
         if not result:
-            print 'something went wrong, please check your API key'
+            print('something went wrong, please check your API key')
             return False
         else:
-            print 'Test entry transmitted correctly'
+            print('Test entry transmitted correctly')
         return True
 
     @classmethod
@@ -70,11 +70,11 @@ class CommandRouter(object):
         ini_path = os.environ.get('APPENLIGHT_INI')
         config = {}
         if not ini_path:
-            print "APPENLIGHT_INI variable is missing from environment/run cmd"
+            print("APPENLIGHT_INI variable is missing from environment/run cmd")
         else:
             config = client.get_config(path_to_config=ini_path)
         if not config.get('appenlight'):
-            print 'WARNING Could not instantiate the client properly'
+            print('WARNING Could not instantiate the client properly')
         else:
             client.Client(config)
         from pyramid.scripts import pserve
@@ -86,7 +86,7 @@ class CommandRouter(object):
 def cli_start():
     args = sys.argv
     if len(args) < 2:
-        print """
+        print ("""
     Possible commands
     makeini [APPENLIGHT_INI_NAME] - creates new config file for appenlight
 
@@ -95,12 +95,12 @@ def cli_start():
     pserve  [APP_CONFIG.ini]      - ensures appenlight client decorates all
                                     libs before pyramid's pserve command is
                                     executed, use instead normal pserve command
-        """
+        """)
         return
     command = args[1]
     command_args = args[2:]
     e_callable = getattr(CommandRouter, command, None)
     if not e_callable:
-        print 'There is no command like %s' % command
+        print('There is no command like %s' % command)
     else:
         e_callable(*command_args)
